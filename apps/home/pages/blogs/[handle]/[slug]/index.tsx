e59caf52065542f1b6bfa4182b73ef5fab4ed8d9
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-01-02 02:13:10 +0330

    client to server side

--- apps/home/pages/blogs/[handle]/[slug]/index.tsx
+++ apps/home/pages/blogs/[handle]/[slug]/index.tsx
@@ -1,9 +1,9 @@
-import { FC, useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+import { FC } from 'react';
 
 import { getArticleByHandleQuery } from '@shopify/graphql-queries';
 import { storefront } from '@shopify/utilities';
 import { ArticleResponseModel } from '@shopify/models';
+import { GetServerSideProps } from 'next';
 
 type ArticleType = {
   handle: string;
@@ -22,73 +22,58 @@ type ArticleType = {
   content: string;
 };
 
-const Article: FC = () => {
-  const router = useRouter();
-  const handle = router.query.handle as string;
-  const slug = router.query.slug as string;
-  const [loading, setLoading] = useState<boolean>(false);
-  const [article, setArticle] = useState<ArticleType>(null);
+type Props = {
+  article: ArticleType;
+};
 
+const Article: FC<Props> = ({ article }: Props) => {
   const date = new Date(article?.publishedAt);
   const generatedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 
-  const getArticle = async () => {
-    setLoading(true);
-
-    const articlesResponse = await storefront<ArticleResponseModel>(
-      getArticleByHandleQuery(handle, slug)
-    );
-    const articleData = articlesResponse.data.blog.articleByHandle;
-    if (articleData) {
-      setArticle(articleData);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getArticle();
-  }, []);
-
-  console.log(article);
   return (
     <div className="relative py-16 bg-white overflow-hidden">
-      {loading ? (
-        <div className="h-96">
-          <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-          </div>
+      <div className="relative px-4 sm:px-6 lg:px-8">
+        <div className="text-lg max-w-prose mx-auto">
+          <h1 className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            {article?.title}
+          </h1>
+          <img
+            className="rounded-lg w-full mt-5"
+            src={article?.image.url}
+            alt={article?.image.altText}
+          />
         </div>
-      ) : (
-        <div className="relative px-4 sm:px-6 lg:px-8">
-          <div className="text-lg max-w-prose mx-auto">
-            <h1 className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              {article?.title}
-            </h1>
-            <img
-              className="rounded-lg w-full mt-5"
-              src={article?.image.url}
-              alt={article?.image.altText}
-            />
-          </div>
 
-          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-            <p>{article?.content}</p>
-            <div className="mt-6">
-              <span className="text-sm font-medium text-gray-900">
-                {article?.authorV2.name}
-              </span>
-              <div className="flex space-x-1 text-sm text-gray-500">
-                {generatedDate}
-              </div>
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <p>{article?.content}</p>
+          <div className="mt-6">
+            <span className="text-sm font-medium text-gray-900">
+              {article?.authorV2.name}
+            </span>
+            <div className="flex space-x-1 text-sm text-gray-500">
+              {generatedDate}
             </div>
           </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const handle = context.query.handle as string;
+  const slug = context.query.slug as string;
+
+  const articleResponse = await storefront<ArticleResponseModel>(
+    getArticleByHandleQuery(handle, slug)
+  );
+  const article = articleResponse.data.blog.articleByHandle;
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
 export default Article;
--- apps/home/pages/blogs/[handle]/index.tsx
+++ apps/home/pages/blogs/[handle]/index.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 
 import { getBlogArticlesByHandleQuery } from '@shopify/graphql-queries';
@@ -23,86 +24,71 @@ type ArticleType = {
   };
 };
 
-const BlogArticles: FC = () => {
+type Props = {
+  articles: ArticleType[];
+};
+
+const BlogArticles: FC<Props> = ({ articles }: Props) => {
   const router = useRouter();
   const handle = router.query.handle as string;
-  const [loading, setLoading] = useState<boolean>(false);
-  const [articles, setArticles] = useState<ArticleType[]>(null);
-
-  const getBlogArticles = async () => {
-    setLoading(true);
-
-    const blogArticlesResponse = await storefront<BlogArticlesResponseModel>(
-      getBlogArticlesByHandleQuery(handle)
-    );
-    const blogArticles = blogArticlesResponse.data.blog.articles.edges;
-
-    if (blogArticles) {
-      setArticles(blogArticles);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getBlogArticles();
-  }, []);
 
   return (
     <div className=" max-w-7xl mx-auto py-12">
       <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-center">
         Articles
       </h2>
-      {loading ? (
-        <div className="h-96">
-          <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-          </div>
-        </div>
-      ) : (
-        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-          {articles?.map((item: ArticleType) => {
-            const article = item.node;
-            const date = new Date(article.publishedAt);
-            const generatedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-            return (
-              <div
-                onClick={() =>
-                  router.push(`/blogs/${handle}/${article.handle}`)
-                }
-                key={article.id}
-                className="flex flex-col rounded-lg shadow-lg overflow-hidden cursor-pointer"
-              >
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-48 w-full object-cover"
-                    src={article.image.url}
-                    alt={article.image.altText}
-                  />
-                </div>
-                <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-                  <span className="text-xl font-semibold text-gray-900 mt-2">
-                    {article.title}
+      <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        {articles?.map((item: ArticleType) => {
+          const article = item.node;
+          const date = new Date(article.publishedAt);
+          const generatedDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+          return (
+            <div
+              onClick={() => router.push(`/blogs/${handle}/${article.handle}`)}
+              key={article.id}
+              className="flex flex-col rounded-lg shadow-lg overflow-hidden cursor-pointer"
+            >
+              <div className="flex-shrink-0">
+                <img
+                  className="h-48 w-full object-cover"
+                  src={article.image.url}
+                  alt={article.image.altText}
+                />
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <span className="text-xl font-semibold text-gray-900 mt-2">
+                  {article.title}
+                </span>
+                <div className="mt-6">
+                  <span className="text-sm font-medium text-gray-900">
+                    {article.authorV2.name}
                   </span>
-                  <div className="mt-6">
-                    <span className="text-sm font-medium text-gray-900">
-                      {article.authorV2.name}
-                    </span>
-                    <div className="flex space-x-1 text-sm text-gray-500">
-                      {generatedDate}
-                    </div>
+                  <div className="flex space-x-1 text-sm text-gray-500">
+                    {generatedDate}
                   </div>
                 </div>
               </div>
-            );
-          })}
-        </div>
-      )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const handle = context.query.handle as string;
+
+  const blogArticlesResponse = await storefront<BlogArticlesResponseModel>(
+    getBlogArticlesByHandleQuery(handle)
+  );
+  const articles = blogArticlesResponse.data.blog.articles.edges;
+
+  return {
+    props: {
+      articles,
+    },
+  };
 };
 
 export default BlogArticles;
