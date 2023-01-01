4d354bb466f250d4ce828a9ab0387c89d466eb01
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2023-01-02 02:05:01 +0330

    blogs ssr

--- apps/home/pages/blogs/index.tsx
+++ apps/home/pages/blogs/index.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
+import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 
 import { blogsQuery } from '@shopify/graphql-queries';
@@ -12,60 +13,48 @@ type BlogType = {
   };
 };
 
-const Blogs: FC = () => {
+type BlogsType = BlogType[];
+
+type Props = {
+  blogs: BlogsType;
+};
+
+const Blogs: FC<Props> = ({ blogs }: Props) => {
   const router = useRouter();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [blogs, setBlogs] = useState<BlogType[]>();
-
-  const getBlogs = async () => {
-    setLoading(true);
-
-    const blogsResponse = await storefront<BlogsResponseModel>(blogsQuery);
-    const blogsData = blogsResponse.data.blogs.edges;
-
-    if (blogsData) {
-      setBlogs(blogsData);
-    }
-
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getBlogs();
-  }, []);
 
   return (
     <div className="max-w-7xl mx-auto py-12">
       <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-center">
         Blogs
       </h2>
-      {loading ? (
-        <div className="h-96">
-          <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-            <div className="w-12 bg-gray-300 h-12 rounded-full "></div>
-          </div>
-        </div>
-      ) : (
-        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-          {blogs?.map((item: BlogType) => {
-            const blog = item.node;
-            return (
-              <div
-                onClick={() => router.push(`/blogs/${blog.handle}`)}
-                key={blog.title}
-                className="rounded-lg shadow-lg overflow-hidden bg-white p-5 text-xl font-semibold text-gray-900 cursor-pointer"
-              >
-                <span>{blog.title}</span>
-              </div>
-            );
-          })}
-        </div>
-      )}
+      <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+        {blogs.map((item: BlogType) => {
+          const blog = item.node;
+          return (
+            <div
+              onClick={() => router.push(`/blogs/${blog.handle}`)}
+              key={blog.title}
+              className="rounded-lg shadow-lg overflow-hidden bg-white p-5 text-xl font-semibold text-gray-900 cursor-pointer"
+            >
+              <span>{blog.title}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const blogsResponse = await storefront<BlogsResponseModel>(blogsQuery);
+
+  const blogs = blogsResponse.data.blogs.edges;
+
+  return {
+    props: {
+      blogs,
+    },
+  };
 };
 
 export default Blogs;
